23a230c8b9f0e977845d86f79d36909580a32389
Lita1929 <77184233+Lita1929@users.noreply.github.com>
2025-06-20 13:44:15 +0700

    Update app.py

--- students_performance/app.py
+++ students_performance/app.py
@@ -85,5 +85,5 @@ with st.expander("View the Raw Data"):
 if st.button('Predict'):
     new_data = data_preprocessing(data=data)
     with st.expander("View the Preprocessed Data"):
-        st.dataframe(data=new_data, width=900, height=50)
+        st.dataframe(data=new_data, width=800, height=10)
     st.write("Students Performance: {}".format(prediction(new_data)))
